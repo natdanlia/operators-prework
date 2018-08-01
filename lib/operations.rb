e5c83d3bf71f1e6aeebf0384e09264e06239bc0d
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-01 14:23:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+  
+  speed > 60 || speed < 40
 end
 
 
 
 def not_safe?(speed)
-	
+	binding.pry 
+	speed > 60 || speed < 40 ? true : false
 end
 	
-
+not_safe(57)
 
